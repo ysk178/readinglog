3180938725e5067543cc277612ce3972d7eb4e43
ysk178 <kk.ss.01tt.ht1985@gmail.com>
2021-01-31 22:05:31 +0900

    ニックネームのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :posts
   has_many :comments
+
+  validates :nickname, presence: true, length: { maximum: 6 }
 end
